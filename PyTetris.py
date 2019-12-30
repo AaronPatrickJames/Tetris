916033e2c439c74f38b1472210ec573ad859a3c6
Aaron James <36738943+AaronPatrickJames@users.noreply.github.com>
2019-12-30 12:13:34 -0500

    Update Generation of new object
    Generates new object (does not save old one on board yet) -- Does generate the save but improperly generated
    
    Look for saving state on board of old object.

--- PyTetris.py
+++ PyTetris.py
@@ -2,9 +2,11 @@ import pygame
 import random
 import os
 import sys
+import copy
 
 gameDisplay = pygame.display.set_mode((600,800))
 pygame.display.set_caption("Tetris")
+BOARDDICT = {}
 clock = pygame.time.Clock()
 BACKFILL = (0,0,0)
 
@@ -21,7 +23,7 @@ class blocks:
         gameDisplay.fill(BACKFILL)
         self.fall_y = 2
         self.y += self.fall_y
-        if self.y>755: self.y = 755
+        if self.y>753: self.y = 753
         
     def move_left(self):
         gameDisplay.fill(BACKFILL)
@@ -44,7 +46,7 @@ class blocks:
         gameDisplay.fill(BACKFILL)
         self.fall_y = 10
         self.y += self.fall_y
-        if self.y>755: self.y = 755
+        if self.y>753: self.y = 753
         
     def place(self):
         pass
@@ -54,6 +56,7 @@ class line_tile(blocks):
 
     def line(self):
         self.color = (90,120,255)#blueish
+        self.name = 1
         if self.state == 0 or self.state == 2:
             pygame.draw.rect(gameDisplay, self.color, [self.x, self.y,45,45])
             pygame.draw.rect(gameDisplay, self.color, [self.x, self.y+50,45,45])
@@ -79,7 +82,40 @@ def key_check(event, shape):
             print("Space Bar")
         if event.key == pygame.K_UP:
             shape.rotate()
-        
+
+#spawns a new shape
+def spawn_new_shape():
+    shape = line_tile()
+    return shape
+
+#Saves the state of locked objects in a dict - BOARDDICT
+def board_save(shape):
+    #make key from length
+    key = len(BOARDDICT)
+    #make value from shape items
+    name = copy.deepcopy(shape.name)
+    color = copy.deepcopy(shape.color)
+    x = copy.deepcopy(shape.x)
+    y = copy.deepcopy(shape.y)
+    state = copy.deepcopy(shape.state)
+    
+    values = [name, color, x, y, state]
+    BOARDDICT.update(key = values)
+    print(BOARDDICT)
+    
+    
+#Changes Lock state, calls new object spawner
+def lock_shape(shape):
+    if shape.y == 753:
+        shape.lock = 1
+
+    #lock Shape on board and call new shape
+    if shape.lock == 1:
+        board_save(shape)
+        shape = spawn_new_shape()
+
+    return(shape)
+    
 #Run Game Loop
 def game_logic_loop():
     shape = line_tile()
@@ -95,6 +131,8 @@ def game_logic_loop():
         pygame.display.update()
         key_check(event, shape)
         shape.time_fall()
+        #RunLockStateCheck
+        shape = lock_shape(shape)
         clock.tick(7)
 
 #make white boarders
