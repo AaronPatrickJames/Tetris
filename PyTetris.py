a0a52e3b5bc3e180aebf5610ca14da887d2a4572
Aaron James <36738943+AaronPatrickJames@users.noreply.github.com>
2019-12-30 14:53:53 -0500

    Update PyTetris.py

--- PyTetris.py
+++ PyTetris.py
@@ -261,6 +261,7 @@ def lock_shape(shape):
     
 #Run Game Loop
 def game_logic_loop():
+    #need 1 piece to start the game - Otherwise everything gets weird
     shape = spawn_new_shape()
     #when false is returned, return score, time
     while True:
